5bec973e0aea6ffe4f53e8ed766acff970141d1a
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-01-24 11:13:02 +0100

    added validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,9 @@ class Contact < ApplicationRecord
   belongs_to :group
   paginates_per 7
   
+  validates :name, :email, :group_id, :phone, presence: true
+  validates :name, length: { in: 2..15 }
+
   def get_avatar
 		hash = Digest::MD5.hexdigest(email.downcase)
 		"https://www.gravatar.com/avatar/#{hash}"
