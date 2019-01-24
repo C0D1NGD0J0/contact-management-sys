eed0ade8b2cf52bc3dbb7669394f853c6a1b1eed
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-01-24 09:51:50 +0100

    create method added for contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,4 +6,24 @@ class ContactsController < ApplicationController
   		@contacts = Contact.page(params[:page])
   	end
   end
+
+  def new 
+  	
+  end
+
+  def create
+  	@contact = Contact.new(contact_params)
+  	if @contact.save
+  		flash[:success] = "Contact has been successfully added."
+  		redirect_to contacts_path
+  	else
+  		flash.now[:danger] = "Oops!, please try again."
+  		render :new
+  	end
+  end
+
+  private
+  	def contact_params
+  		params.require(:contact).permit(:name, :email, :phone, :company, :address, :group_id)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'contacts/index', to: 'contacts#index', as: "contacts"
+  get 'contacts/new', to: 'contacts#new', as: 'new_contact'
+  post 'contacts/create', to: 'contacts#create', as: 'create_contact'
   root to: 'home#index'
 end
