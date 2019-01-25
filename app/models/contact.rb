ce979b35ca98d2b3dfd69b83b957b9245ef7b963
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-01-25 15:08:54 +0100

    refactored code to make use of active_record scope

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,26 +5,32 @@ class Contact < ApplicationRecord
   validates :name, :email, :group_id, :phone, presence: true
   validates :name, length: { in: 2..15 }
 
+  scope :search, -> (query) do
+  	where('name ILIKE :query or company ILIKE :query or email ILIKE :query', query: "%#{query.downcase}%") if query.present?
+  end
+
+  scope :by_group, -> (group_id) { where(group_id: group_id) if group_id.present? }
+
   def get_avatar
 		hash = Digest::MD5.hexdigest(email.downcase)
 		"https://www.gravatar.com/avatar/#{hash}"
   end
 
-  def self.search(query)
-  	if query && !query.empty?
-  		where('name ILIKE ?', "%#{query}%")
-  	else
-  		all
-  	end
-  end
+  # def self.search(query)
+  # 	if query && !query.empty?
+  # 		where('name ILIKE ?', "%#{query}%")
+  # 	else
+  # 		all
+  # 	end
+  # end
 
-  def self.by_group(group_id)
-  	if group_id && !group_id.empty?
-  		where(group_id: group_id)
-  	else
-  		all
-  	end
-  end
+  # def self.by_group(group_id)
+  # 	if group_id && !group_id.empty?
+  # 		where(group_id: group_id)
+  # 	else
+  # 		all
+  # 	end
+  # end
 end
 
 
