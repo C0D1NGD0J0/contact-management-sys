9d3cb4a94c75aa2e780c171157f0d8c2f1af502f
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-01 14:00:25 +0100

    changed devise redirect for user update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: 'auth', controllers: { registrations: :custom_registrations }, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   # get 'contacts/index', to: 'contacts#index', as: "contacts"
   # get 'contacts/new', to: 'contacts#new', as: 'new_contact'
   # get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
