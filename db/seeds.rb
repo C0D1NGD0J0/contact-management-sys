9620ff129eba316437ad4f2e68ca5b3ff42889df
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-01 14:58:54 +0100

    updated seed file to reflect changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,30 @@
-Contact.destroy_all;
-Group.destroy_all;
+Contact.destroy_all
+Group.destroy_all
+User.destroy_all
 
-group_ids = []
+user_ids = []
 
-group_ids << Group.create(name: "Business").id;
-group_ids << Group.create(name: "Family").id;
-group_ids << Group.create(name: "Friends").id;
+user_ids << User.create(name: Faker::Name.name, email: "#{Faker::Name.first_name}@example.com", password: 'password').id
+user_ids << User.create(name: Faker::Name.name, email: "#{Faker::Name.first_name}@example.com", password: 'password').id
+
+p "2 users were created"
+
+group_ids = { user_ids[0] => [], user_ids[1] => []}
+
+group_ids[user_ids[0]] << Group.create(name: "Business", user_id: user_ids[0]).id;
+group_ids[user_ids[0]] << Group.create(name: "Family", user_id: user_ids[0]).id;
+group_ids[user_ids[1]] << Group.create(name: "Friends", user_id: user_ids[1]).id;
+group_ids[user_ids[1]] << Group.create(name: "Work", user_id: user_ids[1]).id;
 
 p "#{group_ids.count} created"
 
-number_of_contacts = 20
+number_of_contacts = 50
 group_count = group_ids.length
 contacts = []
 
 number_of_contacts.times do |x|
+	user_id = user_ids[Random.rand(0...2)]
+
 	new_contact = {
 		name: Faker::Name.name,
 		email: Faker::Internet.safe_email,
@@ -21,7 +32,8 @@ number_of_contacts.times do |x|
 		company: "Business Name--#{x}",
 		address: Faker::Address.full_address,
 		website: "www.#{x}.com",
-		group_id: group_ids[Random.rand(0...group_count)]
+		group_id: group_ids[user_id][Random.rand(0...group_count)],
+		user_id: user_id
 	}
 
 	contacts.push(new_contact)
