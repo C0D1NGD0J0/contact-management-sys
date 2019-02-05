4f93b8613987f9d46c055b6f5ccbe4466b03a6ac
c0d1ngd0j0 <sam.gdouglas@gmail.com>
2019-02-05 10:19:19 +0100

    custom error handling page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_parameters, if: :devise_controller?
 	include Pundit
+	rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+	rescue_from ActiveRecord::RecordNotFound, with: :display_404_error
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	def after_sign_in_path_for(resource)
 		stored_location_for(resource) || contacts_path
 	end
+
+	private
+		def user_not_authorized
+			flash[:danger] = "You are not authorized to perform this action."
+			redirect_to action: :index
+		end
+
+		def display_404_error
+			render template: 'shared/404', status: 404
+		end
 
   protected
 	  def configure_permitted_parameters
